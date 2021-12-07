1aaa9a464e168060b15a7f24549eaaaa0087282e
maniyamman <maniyaman76@gmail.com>
2021-12-07 02:03:57 +0100

    changes to navabr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
 
     const [click , setClick] = useState(false);
 
-    const handleClick = () => setClick(!click)
+    const handleClick = () =>  setClick(!click)
 
     return (
         <>
