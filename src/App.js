8585ee7dec335bbe900552eaa3f4c14c440ff449
maniyamman <maniyaman76@gmail.com>
2021-12-09 04:13:24 +0100

    Navabar and other components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {BrowserRouter , Route , Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route  path='/'  />
-    
+    <Route exact path='/' element={<Home />} />
     </Routes>
   </BrowserRouter>
   );
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,14 @@
+
+export const homeObjOne = {
+    lightBg: false,
+    lighText: true,
+    lightTextDesc: true,
+    topLine: 'Exclusive',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get acces to our exclusive diamond card that allow you to send unlimited',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/svg-1.svg',
+    alt: 'Credit Card'
+
+}
